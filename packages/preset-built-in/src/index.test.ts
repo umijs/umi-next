988296f83addaaba1d4561408d900949516b7f8f
ycjcl868 <45808948@qq.com>
2020-01-17 10:45:21 +0800

    fix: test dev command

--- packages/preset-built-in/src/index.test.ts
+++ packages/preset-built-in/src/index.test.ts
@@ -1,6 +1,6 @@
 import { Service } from '@umijs/core';
 import { join } from 'path';
-import { rimraf } from '@umijs/utils/src';
+import { rimraf, got } from '@umijs/utils';
 import { existsSync, readFileSync } from 'fs';
 
 const fixtures = join(__dirname, 'fixtures');
@@ -52,4 +52,29 @@ test('api.writeTmpFile', async () => {
   const tmpFile = join(cwd, '.umi-test', 'foo');
   expect(readFileSync(tmpFile, 'utf-8')).toEqual('foo');
   rimraf.sync(tmpFile);
+});
+
+test('dev', async () => {
+  const cwd = join(fixtures, 'normal');
+  const service = new Service({
+    cwd,
+    presets: [require.resolve('./index.ts')],
+  });
+  const { port, hostname, listeningApp } = await service.run({
+    name: 'dev',
+  });
+
+  const result = await got(`http://${hostname}:${port}`);
+  expect(result.body).toMatch(/umi\.css/);
+  expect(result.body).toMatch(/umi\.js/);
+  expect(result.statusCode).toEqual(200);
+
+  const absTmpDir = join(cwd, '.umi-test');
+  expect(existsSync(join(absTmpDir, 'umi.ts'))).toEqual(true);
+  expect(existsSync(join(absTmpDir, 'core/history.ts'))).toEqual(true);
+  expect(existsSync(join(absTmpDir, 'core/plugin.ts'))).toEqual(true);
+  expect(existsSync(join(absTmpDir, 'core/routes.ts'))).toEqual(true);
+  expect(existsSync(join(absTmpDir, 'core/umiExports.ts'))).toEqual(true);
+  rimraf.sync(join(absTmpDir));
+  listeningApp.close();
 });
