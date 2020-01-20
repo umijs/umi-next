58c282473dc607b1bcb371b5411f521664cf6fdc
ycjcl868 <45808948@qq.com>
2020-01-20 11:46:58 +0800

    fix: server

--- packages/preset-built-in/src/plugins/commands/dev/mock/createMiddleware.test.ts
+++ packages/preset-built-in/src/plugins/commands/dev/mock/createMiddleware.test.ts
@@ -1,25 +1,24 @@
 import { join } from 'path';
 import { writeFileSync } from 'fs';
-import express from 'express';
 import { Service } from '@umijs/core';
+import { Server } from '@umijs/server';
 import { winPath } from '@umijs/utils';
-import portfinder from 'portfinder';
 import got from 'got';
 import rimraf from 'rimraf';
 import createMiddleware from './createMiddleware';
 import { getMockData } from './utils';
 
 describe('createMiddleware', () => {
-  let port;
-  let server;
   const cwd = winPath(join(__dirname, 'fixtures/createMiddleware'));
   const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
   const HOME_PAGE = 'homepage';
   let watcher = null;
+  let port;
+  let server;
+  let hostname;
 
   beforeAll(async () => {
-    port = await portfinder.getPortPromise({ port: 8000 });
     const service = new Service({
       cwd,
       plugins: [],
@@ -48,23 +47,22 @@ describe('createMiddleware', () => {
         }),
     });
     watcher = middlewareWatcher;
-    return new Promise((resolve, reject) => {
-      const app = express();
-      app.use(middleware);
-      app.use((req, res, next) => {
+    server = new Server({
+      beforeMiddlewares: [middleware],
+      compilerMiddleware: (req, res, next) => {
         if (req.path === '/') {
           res.end(HOME_PAGE);
         } else {
           next();
         }
-      });
-      server = app.listen(port, err => {
-        if (err) {
-          reject(err);
-        }
-        resolve();
-      });
+      },
     });
+    const result = await server.listen({
+      port: 8000,
+      hostname: 'localhost',
+    });
+    port = result.port;
+    hostname = result.hostname;
   });
 
   afterAll(() => {
@@ -72,29 +70,29 @@ describe('createMiddleware', () => {
   });
 
   it('get', async () => {
-    const { body } = await got(`http://localhost:${port}/api/a`);
+    const { body } = await got(`http://${hostname}:${port}/api/a`);
     expect(body).toEqual(`{"a":1}`);
   });
 
   it('post', async () => {
-    const { body } = await got(`http://localhost:${port}/api/a`, {
+    const { body } = await got(`http://${hostname}:${port}/api/a`, {
       method: 'post',
     });
     expect(body).toEqual(`{"a":1}`);
   });
 
   it('function handler', async () => {
-    const { body } = await got(`http://localhost:${port}/api/b`);
+    const { body } = await got(`http://${hostname}:${port}/api/b`);
     expect(body).toEqual(`{"b":1}`);
   });
 
   it('fallback to next', async () => {
-    const { body } = await got(`http://localhost:${port}/`);
+    const { body } = await got(`http://${hostname}:${port}/`);
     expect(body).toEqual(HOME_PAGE);
   });
 
   it('params', async () => {
-    const { body } = await got(`http://localhost:${port}/api/users/1`);
+    const { body } = await got(`http://${hostname}:${port}/api/users/1`);
     expect(body).toEqual(`{"a":1}`);
   });
 
@@ -102,7 +100,7 @@ describe('createMiddleware', () => {
     const absTmpFile = winPath(join(cwd, 'mock/tmp.js'));
     writeFileSync(absTmpFile, `export default {'/api/tmp': {tmp:1}}`, 'utf-8');
     await delay(500);
-    const { body } = await got(`http://localhost:${port}/api/tmp`);
+    const { body } = await got(`http://${hostname}:${port}/api/tmp`);
     expect(body).toEqual(`{"tmp":1}`);
     rimraf.sync(absTmpFile);
   });
@@ -115,6 +113,6 @@ describe('createMiddleware', () => {
   });
 
   afterAll(() => {
-    if (server) server.close();
+    server.listeningApp?.close();
   });
 });
