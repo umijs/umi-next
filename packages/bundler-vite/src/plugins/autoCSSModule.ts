210ebdb357adf960d40d48ea04744fa3a30cac1b
lyl444@qq.com <liuyonglang.lyl@alibaba-inc.com>
2021-10-22 16:01:50 +0800

    feat: autoCSSModule plugin

--- packages/bundler-vite/src/plugins/autoCSSModule.ts
+++ packages/bundler-vite/src/plugins/autoCSSModule.ts
@@ -9,6 +9,7 @@
       const REG_EXP = /(import [a-z]+ from ["'].+\.[css|less|sass|scss|styl|stylus|pcss|postcss]+)(["'])/;
 
       if (code.match(REG_EXP)) {
+        // @ts-ignore
         result = code.replace(REG_EXP, ($1, $2, $3) => {
           // add cssModule flag to match cssModuleRE. see vite/src/node/plugins/css.ts
           return `${$2}?.module.css${$3}`;
@@ -17,6 +18,7 @@
 
       return {
         code: result,
+        map: null,
       };
     },
   };
