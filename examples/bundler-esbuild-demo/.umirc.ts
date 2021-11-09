6123b59da68c4177180e86e36c72e4b68ee92af8
xiaohuoni <448627663@qq.com>
2021-11-09 18:00:43 +0800

    feat: esbuild less

---
+++ examples/bundler-esbuild-demo/.umirc.ts
@@ -0,0 +1,17 @@
+export default {
+  // externals: {
+  //   react: 'React',
+  //   'react-dom': 'ReactDOM',
+  // },
+  chainWebpack(memo: any) {
+    memo;
+  },
+  mfsu: {
+    esbuild: true,
+  },
+  // fastRefresh: false,
+  // favicon: 'https://sivers.com/favicon.ico',
+  headScripts: [`console.log('head script')`],
+  scripts: [`console.log('script')`],
+  npmClient: 'pnpm',
+};
---
+++ examples/bundler-esbuild-demo/pages/index.tsx
@@ -0,0 +1,12 @@
+import 'antd/es/button/style/index.less';
+import React from 'react';
+//@ts-ignore
+import styles from './index.less';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Ant Design</h1>
+    </div>
+  );
+}
