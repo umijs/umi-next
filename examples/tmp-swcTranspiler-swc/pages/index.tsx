7be4565686e945440aba3dd772c5fc479adc96f6
renyou.hdd <renyou.hdd@alibaba-inc.com>
2021-11-11 20:50:02 +0800

    chore: example update

--- examples/tmp-swcTranspiler-swc/pages/index.tsx
+++ examples/tmp-swcTranspiler-swc/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './global.less';
 // @ts-ignore
-// import styles from './index.less';
+import styles from './index.less';
 
 // @ts-ignore
-const styles = await import('./index.less');
+// const styles = await import('./index.less');
 
 export default function HomePage() {
   return <div className={styles.title}>HomePage</div>;
