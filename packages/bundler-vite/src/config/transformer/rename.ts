80c396e059b3b514488d34800259d4d0fc07ffe0
renyou.hdd <renyou.hdd@alibaba-inc.com>
2021-11-15 14:14:55 +0800

    fix: css transformer add resolve alias

--- packages/bundler-vite/src/config/transformer/rename.ts
+++ packages/bundler-vite/src/config/transformer/rename.ts
@@ -24,19 +24,7 @@ function setConfigByPath(config: any, path: string, value: any) {
     const isLastVar = i === fields.length - 1;
 
     if (isLastVar) {
-      // to fix less import issue https://github.com/vitejs/vite/issues/2185
-      // need to transform alias object to alias Array like https://vitejs.dev/config/#resolve-alias
-      if (field === 'alias') {
-        memo[field] = [
-          { find: /^~/, replacement: '' },
-          ...Object.keys(value).map((k) => ({
-            find: k,
-            replacement: value[k],
-          })),
-        ];
-      } else {
-        memo[field] = value;
-      }
+      memo[field] = value;
     } else if (!(field in memo)) {
       memo[field] = {};
     }
