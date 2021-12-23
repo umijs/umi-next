6ea14210441a8f2d5e87827490ffa4912ea45ecc
Ydream <995862798@qq.com>
2021-12-23 16:49:58 +0800

    fix(plugin-layout): use event-emitter package to replace events

--- packages/plugins/src/locale.ts
+++ packages/plugins/src/locale.ts
@@ -105,6 +105,7 @@ export default (api: IApi) => {
     const momentPkgPath = dirname(
       require.resolve(`${resolveKey}/package.json`),
     );
+    const EventEmitterPkg = dirname(require.resolve('event-emitter/package'));
 
     const { baseSeparator, baseNavigator, antd, title, useLocalStorage } = api
       .config.locale as ILocaleConfig;
@@ -170,6 +171,7 @@ export default (api: IApi) => {
     api.writeTmpFile({
       path: 'localeExports.ts',
       content: Mustache.render(localeExportsTpl, {
+        EventEmitterPkg,
         BaseSeparator: baseSeparator,
         BaseNavigator: baseNavigator,
         UseLocalStorage: !!useLocalStorage,
