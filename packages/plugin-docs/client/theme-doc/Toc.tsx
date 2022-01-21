9ff8d4507bceec7e3587f0750b1d9c4a8523353c
Yuanlin <im.yuanlinlin@gmail.com>
2022-01-21 17:12:22 +0800

    feat(plugin-docs): Improved TOC transition and added link

--- packages/plugin-docs/client/theme-doc/Toc.tsx
+++ packages/plugin-docs/client/theme-doc/Toc.tsx
@@ -12,14 +12,19 @@ export default () => {
   const titles = route.titles.filter((t: any) => t.level > 1);
   return (
     <div
-      className="w-full lg:mx-12 mb-12 border
+      className="w-full lg:m-12 mb-12 border
       border-gray-100 p-8 rounded-xl z-20"
     >
       <p className="text-lg font-extrabold">{route.titles[0].title}</p>
       <ul>
         {titles.map((item: any) => {
           return (
-            <li className="mt-3 text-gray-600 cursor-pointer">{item.title}</li>
+            <li
+              className="mt-3 text-gray-600 cursor-pointer
+              hover:text-blue-500 transition duration-300"
+            >
+              <a href={'#' + item.title}>{item.title}</a>
+            </li>
           );
         })}
       </ul>
