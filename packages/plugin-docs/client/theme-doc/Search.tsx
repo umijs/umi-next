4693705fea92bc10fa28373eaf7af0ed9c7f9ba2
Yuanlin <im.yuanlinlin@gmail.com>
2022-01-24 14:56:55 +0800

    feat(plugin-docs-menu): Added hamburger menu for mobile screen

--- packages/plugin-docs/client/theme-doc/Search.tsx
+++ packages/plugin-docs/client/theme-doc/Search.tsx
@@ -12,7 +12,7 @@ export default () => {
 
   return (
     <div
-      className="rounded-lg w-64 flex items-center pr-2 flex-row hover:bg-gray-50
+      className="rounded-lg w-40 lg:w-64 flex items-center pr-2 flex-row hover:bg-gray-50
      transition duration-300 bg-gray-100 border border-white focus-within:border-gray-100
      focus-within:bg-white"
     >
