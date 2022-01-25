30d9da5460fef3f3b7ba891077358bdfbbd6c1fa
Yuanlin <im.yuanlinlin@gmail.com>
2022-01-25 18:18:34 +0800

    feat(plugin-docs): use remark-slug instead of regex replace for heading anchors

--- packages/plugin-docs/src/compiler.ts
+++ packages/plugin-docs/src/compiler.ts
@@ -1,9 +1,10 @@
 // @ts-ignore
 import mdx from '@mdx-js/mdx';
+import remarkSlug from 'remark-slug';
 
 export async function compile(opts: { content: string }) {
   let result = await mdx(opts.content, {
-    remarkPlugins: [],
+    remarkPlugins: [remarkSlug],
     rehypePlugins: [],
     compilers: [],
   });
@@ -15,17 +16,5 @@ ${result}`;
   const title = result.match(/<h1>{`(.*?)`}<\/h1>/)[1];
   result = `${result}\nMDXContent.title = '${title}';`;
 
-  // Inject anchor for headings, so TOC can link to it
-  result = result.replace(
-    /<h([0-9])>{`(.*)`}<\/h([0-9])>/g,
-    `<a class='anchor' id='$2' />
-<h$1>$2</h$3>`,
-  );
-
-  return {
-    result,
-    meta: {
-      title,
-    },
-  };
+  return { result };
 }
