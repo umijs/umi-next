84db4ad795ed29b4c248d7aed1c174f6d67c7316
Yuanlin <im.yuanlinlin@gmail.com>
2022-01-25 11:12:47 +0800

    feat(plugin-docs): Added anchor for headings, let TOC can link to them

--- packages/plugin-docs/src/compiler.ts
+++ packages/plugin-docs/src/compiler.ts
@@ -14,6 +14,14 @@ ${result}`;
   result = result.replace('/* @jsx mdx */', '');
   const title = result.match(/<h1>{`(.*?)`}<\/h1>/)[1];
   result = `${result}\nMDXContent.title = '${title}';`;
+
+  // Inject anchor for headings, so TOC can link to it
+  result = result.replace(
+    /<h([0-9])>{`(.*)`}<\/h([0-9])>/g,
+    `<a class='anchor' id='$2' />
+<h$1>$2</h$3>`,
+  );
+
   return {
     result,
     meta: {
