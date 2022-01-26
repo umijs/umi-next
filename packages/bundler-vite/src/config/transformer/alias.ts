7dae681fd6a95ea720f1324d29b925ac1c296add
晚兮 <wh365686@alibaba-inc.com>
2022-01-26 12:33:58 +0800

    feat(bundler-vite): resolving config overrides

--- packages/bundler-vite/src/config/transformer/alias.ts
+++ packages/bundler-vite/src/config/transformer/alias.ts
@@ -1,22 +1,34 @@
+import type { Alias } from 'vite';
 import type { IConfigProcessor } from '.';
 
 /**
  * transform umi alias to vite alias
  */
-export default (function alias(userConfig) {
-  const config: ReturnType<IConfigProcessor> = {};
 
-  if (typeof userConfig.alias === 'object') {
-    config.resolve = {
-      alias: Object.entries<string>(userConfig.alias).map(([name, target]) => ({
+export default (function alias(userConfig) {
+  const config: ReturnType<IConfigProcessor> = {
+    resolve: {
+      alias: [
+        // to support less-loader ~ for local deps, refer: https://github.com/vitejs/vite/issues/2185
+        { find: /^~/, replacement: '' },
+      ],
+    },
+  };
+
+  // alias: { foo: bar }  foo/hoo > bar/foo
+  // alias: { foo$: bar } foo/hoo > foo/hoo
+  if (userConfig.alias) {
+    const suffix = Object.entries<string>(userConfig.alias).map(
+      ([name, target]) => ({
         // supports webpack suffix $ and less-loader prefix ~
         // example:
         //   - dep => ^~?dep(?=\/|$)
         //   - dep$ => ^~?dep$
         find: new RegExp(`^~?${name.replace(/(?<!\$)$/, '(?=/|$)')}`),
         replacement: target,
-      })),
-    };
+      }),
+    );
+    (config.resolve?.alias as Alias[]).push(...suffix);
   }
 
   return config;
