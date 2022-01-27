0030cd7025ebf1f7065b305c8a8edc6d0113621c
晚兮 <wh365686@alibaba-inc.com>
2022-01-27 14:12:59 +0800

    feat(bundler-vite): add legacyBrowser judge

--- packages/bundler-vite/src/config/transformer/target.ts
+++ packages/bundler-vite/src/config/transformer/target.ts
@@ -1,13 +1,13 @@
+import type { Options } from '@vitejs/plugin-legacy';
 import legacyPlugin from '@vitejs/plugin-legacy';
-// @ts-ignore
-import polyfill from 'rollup-plugin-polyfill';
 import type { IConfigProcessor } from '.';
+import { getBrowserlist } from './css';
 
 /**
  * transform umi targets to vite build.target
  */
 export default (function target(userConfig) {
-  const config: ReturnType<IConfigProcessor> = { build: {} };
+  const config: ReturnType<IConfigProcessor> = { build: {}, plugins: [] };
 
   // convert { ie: 11 } to ['ie11']
   if (typeof userConfig.targets === 'object') {
@@ -15,26 +15,42 @@ export default (function target(userConfig) {
       ([name, ver]) => `${name}${ver}`,
     );
   }
+  const LEGACY_BROWSERS: Record<string, number> = {
+    ie: 12,
+    edge: 16,
+    firefox: 60,
+    chrome: 61,
+    safari: 11,
+    opera: 48,
+    ios: 11,
+  };
 
+  // targets: {} > false
+  // targets: { edge: 11 } > true
+  // targets: { edge: 20 } > false
   // refer: https://caniuse.com/?search=esm
-  if (
-    userConfig.targets &&
-    (userConfig.targets.ie <= 11 ||
-      userConfig.targets.edge < 16 ||
-      userConfig.targets.firefox < 60 ||
-      userConfig.targets.chrome < 61 ||
-      userConfig.targets.safari < 11 ||
-      userConfig.targets.opera < 48 ||
-      userConfig.targets.ios < 11)
-  ) {
-    if (userConfig.targets.ie <= 11) {
-      config.plugins?.push(
-        legacyPlugin({
-          additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        }),
-      );
+  function isLegacyBrowser(targets: Record<string, number>) {
+    for (const browserName of Object.keys(targets)) {
+      const version = targets[browserName];
+      if (
+        version &&
+        LEGACY_BROWSERS[browserName] &&
+        version < LEGACY_BROWSERS[browserName]
+      ) {
+        return true;
+      }
     }
-    config.plugins?.push(legacyPlugin({}), polyfill({}));
+    return false;
+  }
+  if (isLegacyBrowser(userConfig.targets)) {
+    const legacyOpts: Options = {
+      targets: getBrowserlist(userConfig.targets),
+      // polyfills: []
+    };
+    if (userConfig.targets.ie <= 11) {
+      legacyOpts.polyfills = ['regenerator-runtime/runtime'];
+    }
+    config.plugins!.push(legacyPlugin(legacyOpts));
   }
 
   return config;
