8cc0cd9fefeca29fefcef58b43d35dcae588eb39
fz6m <59400654+fz6m@users.noreply.github.com>
2022-01-28 13:41:03 +0800

    example(swc): specified config for check output polyfill

--- examples/tmp-swcTranspiler-swc/.umirc.ts
+++ examples/tmp-swcTranspiler-swc/.umirc.ts
@@ -1,4 +1,8 @@
 export default {
   srcTranspiler: 'swc',
-  mfsu: {},
+  targets: {
+    // chrome 53 is last not support async version
+    // https://caniuse.com/async-functions
+    chrome: 54,
+  },
 };
