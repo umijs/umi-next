7a14ce33ff12009383dd7a03756d143430f79568
fz6m <59400654+fz6m@users.noreply.github.com>
2022-02-07 20:46:02 +0800

    fix(swc): auto css plugin visit js file error

--- packages/bundler-webpack/src/swcPlugins/autoCSSModules.ts
+++ packages/bundler-webpack/src/swcPlugins/autoCSSModules.ts
@@ -1,10 +1,17 @@
-import { ImportDeclaration, TsType, VariableDeclaration } from '@swc/core';
+import type { ImportDeclaration, ModuleItem, TsType } from '@swc/core';
 import Visitor from '@swc/core/Visitor';
 import { isStyleFile } from '@umijs/utils';
 
 class AutoCSSModule extends Visitor {
   visitTsType(expression: TsType) {
     return expression;
+  }
+
+  visitModuleItem(n: ModuleItem) {
+    if (n.type === 'ImportDeclaration') {
+      return this.visitImportDeclaration(n);
+    }
+    return n;
   }
 
   visitImportDeclaration(expression: ImportDeclaration): ImportDeclaration {
@@ -20,28 +27,6 @@ class AutoCSSModule extends Visitor {
         },
       };
     }
-    return expression;
-  }
-
-  visitVariableDeclaration(
-    expression: VariableDeclaration,
-  ): VariableDeclaration {
-    const { declarations } = expression;
-    if (
-      declarations.length &&
-      declarations[0].init &&
-      declarations[0].init.type === 'AwaitExpression' &&
-      declarations[0].init.argument.type === 'CallExpression' &&
-      declarations[0].init.argument.arguments.length &&
-      declarations[0].init.argument.arguments[0].expression.type ===
-        'StringLiteral' &&
-      isStyleFile({
-        filename: declarations[0].init.argument.arguments[0].expression.value,
-      })
-    ) {
-      declarations[0].init.argument.arguments[0].expression.value = `${declarations[0].init.argument.arguments[0].expression.value}?modules`;
-    }
-
     return expression;
   }
 }
