6d15a0ebf8a3e1cfbfa0ec9917fcdc87bf434624
晚兮 <wh365686@alibaba-inc.com>
2022-03-02 14:13:54 +0800

    feat(bundler-vite): fix targets type

--- packages/bundler-vite/src/config/transformer/target.ts
+++ packages/bundler-vite/src/config/transformer/target.ts
@@ -39,7 +39,7 @@ export default (function target(userConfig) {
   }
   if (userConfig.targets && isLegacyBrowser(userConfig.targets)) {
     const legacyOpts: Options = {
-      targets: getBrowserlist(userConfig.targets),
+      targets: getBrowserlist(userConfig.targets) as string | string[],
       polyfills: false,
       ignoreBrowserslistConfig: true,
     };
