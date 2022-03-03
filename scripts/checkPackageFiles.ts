6d8b3418649dfad09f09636713c04e1205bca7d1
fz6m <59400654+fz6m@users.noreply.github.com>
2022-03-04 02:37:00 +0800

    ci: add `examples/*` package json check

--- scripts/checkPackageFiles.ts
+++ scripts/checkPackageFiles.ts
@@ -1,11 +1,34 @@
-import path from 'path';
+import 'zx/globals';
 import { eachPkg, getPkgs } from './utils';
 
-const cwd = process.cwd();
-eachPkg(getPkgs(), (opts) => {
-  console.log('Checking package:', opts.pkg);
-  const pkg = require(path.join(cwd, 'packages', opts.pkg, 'package.json'));
-  pkg.files;
+// check packages/*
+eachPkg(getPkgs(), ({ name, pkgJson }) => {
+  console.log(`Checking ${chalk.cyan('package')}:`, name);
+  pkgJson.files;
   // TODO
   // 检测 pkg.files 是否包含必要的和当前 pkg 下的文件和目录
 });
+
+// check examples/*
+const EXAMPLE_DIR = path.join(__dirname, '../examples');
+eachPkg(
+  getPkgs({ base: EXAMPLE_DIR }),
+  ({ name, pkgJson, pkgPath }) => {
+    console.log(`Checking ${chalk.blue('example')}:`, name);
+    const expectName = `@example/${name}`;
+    if (pkgJson.name !== expectName) {
+      pkgJson.name = expectName;
+      console.log(
+        chalk.yellow(`Change '${name}' example name to '${expectName}'`),
+      );
+    }
+    if (pkgJson.private !== true) {
+      pkgJson.private = true;
+      console.log(chalk.yellow(`Set '${name}' example as private pacakge`));
+    }
+    fs.writeFileSync(pkgPath, `${JSON.stringify(pkgJson, null, 2)}\n`, 'utf-8');
+  },
+  {
+    base: EXAMPLE_DIR,
+  },
+);
