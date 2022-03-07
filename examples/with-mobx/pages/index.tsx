966fae74669bf5f4657f16047d941dfda357b927
cnyballk <584518260@qq.com>
2022-03-07 20:30:27 +0800

    style: style is unified with Valtio

--- examples/with-mobx/pages/index.tsx
+++ examples/with-mobx/pages/index.tsx
@@ -6,13 +6,11 @@ export default inject('counter')(
   observer(function HomePage(props) {
     return (
       <div className="container">
+        <p className="title">UmiJS x Mobx</p>
+        <p className="display-count">{props.counter.count}</p>
         <div>
-          <p className="title">UmiJS x mobx</p>
-          <div>
-            <p className="count">count: {props.counter.count}</p>
-            <button onClick={() => props.counter.increase()}>+</button>
-            <button onClick={() => props.counter.decrease()}>-</button>
-          </div>
+          <button onClick={() => props.counter.increase()}>+</button>
+          <button onClick={() => props.counter.decrease()}>-</button>
         </div>
       </div>
     );
