7e4675b4c1652fcbdda72d859f32a08e961390eb
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-03-09 18:13:22 +0800

    feat(api-routes): Add comment for matchApiRoute()

--- packages/preset-umi/src/features/apiRoute/utils.ts
+++ packages/preset-umi/src/features/apiRoute/utils.ts
@@ -12,7 +12,16 @@ export function esbuildIgnorePathPrefixPlugin() {
   };
 }
 
-// 匹配 API 路由，包含动态路由的处理, 前后 trailing slash 的处理及 /api 前缀的处理
+/**
+ * 匹配 API 路由，包含动态路由的处理, 前后 trailing slash 的处理及 /api 前缀的处理
+ *
+ * 例如：`/api/users/ken20001207`, `api/users/ken20001207`,
+ * `/api/users/ken20001207/`, `api/users/ken20001207/`, `users/ken20001207/`,
+ * `/users/ken20001207/`, `users/ken20001207`, `/users/ken20001207` 等请求
+ *
+ * 都会被匹配到 `/api/users/[userId]` 这个路由中,
+ * 并且 `ken20001207` 会被抽取出来放入 `params.userId` 中
+ */
 export function matchApiRoute(
   apiRoutes: IRoute[],
   path: string,
