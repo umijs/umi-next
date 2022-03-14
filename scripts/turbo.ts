e791143b048bacb05dddfdc13f4668310893d57f
Yingci <fz6meng@gmail.com>
2022-03-14 07:22:35 +0000

    test: rerun tests

--- scripts/turbo.ts
+++ scripts/turbo.ts
@@ -1,6 +1,6 @@
-import * as logger from '@umijs/utils/src/logger';
 import spawn from '@umijs/utils/compiled/cross-spawn';
 import yArgs from '@umijs/utils/compiled/yargs-parser';
+import * as logger from '@umijs/utils/src/logger';
 import { join } from 'path';
 
 (async () => {
@@ -45,9 +45,9 @@ async function turbo(opts: {
   cache?: boolean;
   parallel?: boolean;
 }) {
-  const extraCmd = opts.extra ? `-- -- ${opts.extra}` : '';
   const cacheCmd = opts.cache === false ? '--no-cache --force' : '';
   const parallelCmd = opts.parallel ? '--parallel' : '';
+  const extraCmd = opts.extra ? `-- -- ${opts.extra}` : '';
 
   const options = [
     opts.cmd,
