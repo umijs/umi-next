cb728d5d87c9109c7f11c07b1c182c2c8f820a49
pshu <pishu.spf@antfin.com>
2022-03-15 23:49:52 +0800

    refactor(plugins): 🎨 css related plugin only run in dev or build

--- packages/plugins/src/tailwindcss.ts
+++ packages/plugins/src/tailwindcss.ts
@@ -1,6 +1,6 @@
-import { crossSpawn, winPath } from 'umi/plugin-utils';
 import { join } from 'path';
 import { IApi } from 'umi';
+import { crossSpawn, winPath } from 'umi/plugin-utils';
 
 export default (api: IApi) => {
   api.describe({
@@ -16,33 +16,35 @@ export default (api: IApi) => {
   let tailwind: any = null;
   const outputPath = 'plugin-tailwindcss/tailwind.css';
 
-  api.onStart(() => {
-    const inputPath = join(api.cwd, 'tailwind.css');
-    const generatedPath = join(api.paths.absTmpPath, outputPath);
-    const binPath = join(api.cwd, 'node_modules/.bin/tailwind');
+  if (api.name === 'dev' || api.name === 'build') {
+    api.onStart(() => {
+      const inputPath = join(api.cwd, 'tailwind.css');
+      const generatedPath = join(api.paths.absTmpPath, outputPath);
+      const binPath = join(api.cwd, 'node_modules/.bin/tailwind');
 
-    /** 透过子进程建立 tailwindcss 服务，将生成的 css 写入 generatedPath */
-    tailwind = crossSpawn(
-      `${binPath}`,
-      [
-        '-i',
-        inputPath,
-        '-o',
-        generatedPath,
-        api.env === 'development' ? '--watch' : '',
-      ],
-      {
-        stdio: 'inherit',
-      },
-    );
-    tailwind.on('error', (m: any) => {
-      api.logger.error('tailwindcss service encounter an error: ' + m);
+      /** 透过子进程建立 tailwindcss 服务，将生成的 css 写入 generatedPath */
+      tailwind = crossSpawn(
+        `${binPath}`,
+        [
+          '-i',
+          inputPath,
+          '-o',
+          generatedPath,
+          api.env === 'development' ? '--watch' : '',
+        ],
+        {
+          stdio: 'inherit',
+        },
+      );
+      tailwind.on('error', (m: any) => {
+        api.logger.error('tailwindcss service encounter an error: ' + m);
+      });
     });
-  });
 
-  /** 将生成的 css 文件加入到 import 中 */
-  api.addEntryImports(() => {
-    const generatedPath = winPath(join(api.paths.absTmpPath, outputPath));
-    return [{ source: generatedPath }];
-  });
+    /** 将生成的 css 文件加入到 import 中 */
+    api.addEntryImports(() => {
+      const generatedPath = winPath(join(api.paths.absTmpPath, outputPath));
+      return [{ source: generatedPath }];
+    });
+  }
 };
--- packages/plugins/src/unocss.ts
+++ packages/plugins/src/unocss.ts
@@ -17,35 +17,37 @@ export default (api: IApi) => {
     enableBy: api.EnableBy.config,
   });
 
-  const outputPath = 'uno.css';
+  if (api.name === 'dev' || api.name === 'build') {
+    const outputPath = 'uno.css';
 
-  api.onStart(() => {
-    /** 由于 @unocss/cli 对设置文件进行了检查，因此加入需要 unocss.config.ts 设置的提示
-     * https://github.com/antfu/unocss/blob/main/packages/cli/src/index.ts#L93 */
-    if (!existsSync(join(api.paths.cwd, 'unocss.config.ts')))
-      api.logger.warn(
-        '请在项目目录中添加 unocss.config.ts 文件，并配置需要的 unocss presets，否则插件将没有效果！',
+    api.onStart(() => {
+      /** 由于 @unocss/cli 对设置文件进行了检查，因此加入需要 unocss.config.ts 设置的提示
+       * https://github.com/antfu/unocss/blob/main/packages/cli/src/index.ts#L93 */
+      if (!existsSync(join(api.paths.cwd, 'unocss.config.ts')))
+        api.logger.warn(
+          '请在项目目录中添加 unocss.config.ts 文件，并配置需要的 unocss presets，否则插件将没有效果！',
+        );
+
+      const generatedPath = join(api.paths.absTmpPath, outputPath);
+      const binPath = join(api.cwd, 'node_modules/.bin/unocss');
+      const watchDirs = api.config.unocss.watch;
+
+      /** 透过子进程建立 unocss 服务，将生成的 css 写入 generatedPath */
+      const unocss = exec(
+        `${binPath} ${watchDirs.join(' ')} --out-file ${generatedPath} ${
+          api.env === 'development' ? '--watch' : ''
+        }`,
+        { cwd: api.cwd },
       );
 
-    const generatedPath = join(api.paths.absTmpPath, outputPath);
-    const binPath = join(api.cwd, 'node_modules/.bin/unocss');
-    const watchDirs = api.config.unocss.watch;
-
-    /** 透过子进程建立 unocss 服务，将生成的 css 写入 generatedPath */
-    const unocss = exec(
-      `${binPath} ${watchDirs.join(' ')} --out-file ${generatedPath} ${
-        api.env === 'development' ? '--watch' : ''
-      }`,
-      { cwd: api.cwd },
-    );
-
-    unocss.on('error', (m: any) => {
-      api.logger.error('unocss service encounter an error: ' + m);
+      unocss.on('error', (m: any) => {
+        api.logger.error('unocss service encounter an error: ' + m);
+      });
     });
-  });
-  /** 将生成的 css 文件加入到 import 中 */
-  api.addEntryImports(() => {
-    const generatedPath = winPath(join(api.paths.absTmpPath, outputPath));
-    return [{ source: generatedPath }];
-  });
+    /** 将生成的 css 文件加入到 import 中 */
+    api.addEntryImports(() => {
+      const generatedPath = winPath(join(api.paths.absTmpPath, outputPath));
+      return [{ source: generatedPath }];
+    });
+  }
 };
