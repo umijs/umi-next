b2c8a727820c6c213520c96bb4e502e258b742e7
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-03-16 16:04:02 +0800

    fix(api-route): remove unused dynamic routes config (Vercel will take care of it)

--- packages/preset-umi/src/features/apiRoute/vercel/esbuild.ts
+++ packages/preset-umi/src/features/apiRoute/vercel/esbuild.ts
@@ -1,16 +1,8 @@
 import esbuild from '@umijs/bundler-utils/compiled/esbuild';
-import fs from 'fs';
 import { join, resolve } from 'path';
 import type { IApi, IRoute } from '../../../types';
 import { OUTPUT_PATH } from '../constants';
 import { esbuildIgnorePathPrefixPlugin } from '../utils';
-
-interface VercelDynamicRouteConfig {
-  page: string;
-  regex: string;
-  routeKeys: { [key: string]: string };
-  namedRegex: string;
-}
 
 // 将 API 路由的临时文件打包为 Vercel 的 Serverless Function 可以使用的格式
 export default async function (api: IApi, apiRoutes: IRoute[]) {
@@ -32,61 +24,4 @@ export default async function (api: IApi, apiRoutes: IRoute[]) {
     plugins: [esbuildIgnorePathPrefixPlugin()],
     external: [...Object.keys(pkg.dependencies || {})],
   });
-
-  const dynamicRoutes: VercelDynamicRouteConfig[] = [];
-
-  apiRoutes.map((r) => {
-    if (r.path.match(/\[.*]/)) {
-      const keys = r.path
-        .match(/\[.*?]/g)
-        ?.map((k) => (k = k.replace(/[\[\]]/g, '')));
-      const routeKeys: { [key: string]: string } = {};
-      keys?.map((k) => {
-        routeKeys[k] = k;
-      });
-      dynamicRoutes.push({
-        page: '/api/' + r.path,
-        regex: getVercelDynamicRoutesRegex(r.path),
-        routeKeys,
-        namedRegex: getVercelDynamicRoutesNamedRegex(r.path),
-      });
-    }
-  });
-
-  fs.writeFileSync(
-    join(api.paths.cwd, '.output/routes-manifest.json'),
-    JSON.stringify({ version: 3, dynamicRoutes }, null, 2),
-  );
-}
-
-/**
- *  按照 Vercel 的要求将动态路由进行转换
- *
- *  若传入的 path 是
- *  `/users/[userId]/repos/[repoId]`
- *
- *  则返回的 regex 为：
- *  `^/api/users/([^/]+?)/repos/([^/]+?)(?:/)?$`
- *
- *  这个 regex 是给 Vercel 用于匹配 Serverless functions 请求的
- *  参考：https://vercel.com/docs/file-system-api#configuration/routes
- */
-function getVercelDynamicRoutesRegex(path: string) {
-  return '^/api/' + path.replace(/\[.*?]/g, '([^/]+?)') + '(?:/)?$';
-}
-
-/**
- *  按照 Vercel 的要求将动态路由进行转换，并且将匹配组加入别名
- *
- *  若传入的 path 是
- *  `/users/[userId]/repos/[repoId]`
- *
- *  则返回的 regex 为：
- *  `^/api/users/(?<userId>[^/]+?)/repos/(?<repoId>[^/]+?)(?:/)?$`
- *
- *  这个 regex 是给 Vercel 用于匹配 Serverless functions 请求的
- *  参考：https://vercel.com/docs/file-system-api#configuration/routes
- */
-function getVercelDynamicRoutesNamedRegex(path: string) {
-  return '^/api/' + path.replace(/\[(.*?)]/g, '(?<$1>[^/]+?)') + '(?:/)?$';
 }
