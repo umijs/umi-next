42634d3deae9b63bdc2917a172c0ad5b5ca1b4f9
pshu <pishu.spf@antfin.com>
2022-03-16 10:39:33 +0800

    refactor: 🎨 move "if" into onStart hook to make plugin behavior stable

--- packages/plugins/src/tailwindcss.ts
+++ packages/plugins/src/tailwindcss.ts
@@ -16,35 +16,37 @@ export default (api: IApi) => {
   let tailwind: any = null;
   const outputPath = 'plugin-tailwindcss/tailwind.css';
 
-  if (api.name === 'dev' || api.name === 'build') {
-    api.onStart(() => {
-      const inputPath = join(api.cwd, 'tailwind.css');
-      const generatedPath = join(api.paths.absTmpPath, outputPath);
-      const binPath = join(api.cwd, 'node_modules/.bin/tailwind');
+  api.onStart(() => {
+    if (api.name !== 'dev' && api.name !== 'build') {
+      return;
+    }
 
-      /** 透过子进程建立 tailwindcss 服务，将生成的 css 写入 generatedPath */
-      tailwind = crossSpawn(
-        `${binPath}`,
-        [
-          '-i',
-          inputPath,
-          '-o',
-          generatedPath,
-          api.env === 'development' ? '--watch' : '',
-        ],
-        {
-          stdio: 'inherit',
-        },
-      );
-      tailwind.on('error', (m: any) => {
-        api.logger.error('tailwindcss service encounter an error: ' + m);
-      });
-    });
+    const inputPath = join(api.cwd, 'tailwind.css');
+    const generatedPath = join(api.paths.absTmpPath, outputPath);
+    const binPath = join(api.cwd, 'node_modules/.bin/tailwind');
 
-    /** 将生成的 css 文件加入到 import 中 */
-    api.addEntryImports(() => {
-      const generatedPath = winPath(join(api.paths.absTmpPath, outputPath));
-      return [{ source: generatedPath }];
+    /** 透过子进程建立 tailwindcss 服务，将生成的 css 写入 generatedPath */
+    tailwind = crossSpawn(
+      `${binPath}`,
+      [
+        '-i',
+        inputPath,
+        '-o',
+        generatedPath,
+        api.env === 'development' ? '--watch' : '',
+      ],
+      {
+        stdio: 'inherit',
+      },
+    );
+    tailwind.on('error', (m: any) => {
+      api.logger.error('tailwindcss service encounter an error: ' + m);
     });
-  }
+  });
+
+  /** 将生成的 css 文件加入到 import 中 */
+  api.addEntryImports(() => {
+    const generatedPath = winPath(join(api.paths.absTmpPath, outputPath));
+    return [{ source: generatedPath }];
+  });
 };
