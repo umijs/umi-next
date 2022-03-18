ebc84b0c51a5bf6fb368595821e8f73319e71487
Peach <scdzwyxst@gmail.com>
2022-03-18 19:53:47 +0800

    feat(plugins): provide runtime component from access plugin

--- packages/plugins/src/access.ts
+++ packages/plugins/src/access.ts
@@ -49,6 +49,18 @@ import { AccessContext } from './context';
 export const useAccess = () => {
   return React.useContext(AccessContext);
 };
+
+export interface AccessProps {
+  accessible: boolean;
+  fallback?: React.ReactNode;
+}
+export const Access: React.FC<AccessProps> = (props) => {
+  if (process.env.NODE_ENV === 'development' && typeof props.accessible !== 'boolean') {
+    throw new Error('[access] the \`accessible\` property on <Access /> should be a boolean');
+  }
+
+  return props.accessible ? props.children : props.fallback;
+};
       `,
     });
 
