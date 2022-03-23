40019139d2d7f2aa3166ee1a462b4835b10544a3
pshu <pishu.spf@antfin.com>
2022-03-23 11:13:50 +0800

    test: ✅  revert to tdd style test case

--- packages/umi/src/client/plugin.test.ts
+++ packages/umi/src/client/plugin.test.ts
@@ -2,72 +2,70 @@ import { ApplyPluginsType, PluginManager } from './plugin';
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
-describe('PluginManager', function () {
-  it('does NOT wait async calls in async=false mode', async () => {
-    const pm = new PluginManager({
-      validKeys: ['foo'],
-    });
-
-    const asyncCall = jest.fn();
-    const syncCall = jest.fn();
-
-    pm.register({
-      apply: {
-        foo: async () => {
-          await delay(100);
-          asyncCall();
-        },
-      },
-      path: '/a',
-    });
-    pm.register({
-      apply: {
-        foo: syncCall,
-      },
-      path: '/a',
-    });
-
-    await pm.applyPlugins({
-      key: 'foo',
-      type: ApplyPluginsType.event,
-      async: false,
-    });
-
-    expect(syncCall).toBeCalled();
-    expect(asyncCall).not.toBeCalled();
+test('PluginManager#applyPlugins in async=false mode', async () => {
+  const pm = new PluginManager({
+    validKeys: ['foo'],
   });
 
-  it('waits all calls in async=true mode', async () => {
-    const pm = new PluginManager({
-      validKeys: ['foo'],
-    });
+  const asyncCall = jest.fn();
+  const syncCall = jest.fn();
 
-    const asyncCall = jest.fn();
-    const syncCall = jest.fn();
-
-    pm.register({
-      apply: {
-        foo: async () => {
-          await delay(100);
-          asyncCall();
-        },
+  pm.register({
+    apply: {
+      foo: async () => {
+        await delay(100);
+        asyncCall();
       },
-      path: '/a',
-    });
-    pm.register({
-      apply: {
-        foo: syncCall,
-      },
-      path: '/a',
-    });
-
-    await pm.applyPlugins({
-      key: 'foo',
-      type: ApplyPluginsType.event,
-      async: true,
-    });
-
-    expect(syncCall).toBeCalled();
-    expect(asyncCall).toBeCalled();
+    },
+    path: '/a',
   });
+  pm.register({
+    apply: {
+      foo: syncCall,
+    },
+    path: '/a',
+  });
+
+  await pm.applyPlugins({
+    key: 'foo',
+    type: ApplyPluginsType.event,
+    async: false,
+  });
+
+  expect(syncCall).toBeCalled();
+  expect(asyncCall).not.toBeCalled();
+});
+
+test('PluginManager#applyPlugins in async=true mode', async () => {
+  const pm = new PluginManager({
+    validKeys: ['foo'],
+  });
+
+  const asyncCall = jest.fn();
+  const syncCall = jest.fn();
+
+  pm.register({
+    apply: {
+      foo: async () => {
+        await delay(100);
+        asyncCall();
+      },
+    },
+    path: '/a',
+  });
+  pm.register({
+    apply: {
+      foo: syncCall,
+    },
+    path: '/a',
+  });
+
+  await pm.applyPlugins({
+    key: 'foo',
+    type: ApplyPluginsType.event,
+    async: true,
+  });
+
+  expect(syncCall).toBeCalled();
+  expect(asyncCall).toBeCalled();
 });
