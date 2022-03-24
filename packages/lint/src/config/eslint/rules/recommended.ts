b08a165050b5ac91bd8134b3ba234f584cbe8e40
Peach <shengtao.xst@antgroup.com>
2022-03-24 10:18:47 +0800

    chore(lint): add rule sample files

---
+++ packages/lint/src/config/eslint/rules/recommended.ts
@@ -0,0 +1,10 @@
+/**
+ * recommended enabled/disabled rules for umi project
+ * @note  base on recommended rule set from loaded eslint plugins
+ */
+export default {
+  // eslint built-in rules
+  // ...
+  // config-plugin-react rules
+  // ...
+};
