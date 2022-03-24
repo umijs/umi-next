1ef14d02c089ab249b2d8defc2f627a82692b437
fz6m <59400654+fz6m@users.noreply.github.com>
2022-03-24 14:44:53 +0800

    wip: support peer deps redirect in monorepo

--- packages/preset-umi/src/features/monorepo/redirect.ts
+++ packages/preset-umi/src/features/monorepo/redirect.ts
@@ -1,4 +1,4 @@
-import { isMonorepo, logger } from '@umijs/utils';
+import { isMonorepo, logger, resolve } from '@umijs/utils';
 import { pkgUp } from '@umijs/utils/compiled/pkg-up';
 import assert from 'assert';
 import { existsSync, statSync } from 'fs';
@@ -10,8 +10,13 @@ import type { IApi } from '../../types';
 interface IConfigs {
   srcDir?: string[];
   exclude?: RegExp[];
+  peerDeps?: boolean;
 }
 
+/**
+ * default redirect react core deps in `peerDeps` scene
+ */
+const CORE_DEPS = ['react', 'react-dom', 'react-router', 'react-router-dom'];
 export default (api: IApi) => {
   api.describe({
     key: 'monorepoRedirect',
@@ -22,6 +27,7 @@ export default (api: IApi) => {
           Joi.object({
             srcDir: Joi.array().items(Joi.string()),
             exclude: Joi.array().items(Joi.object().instance(RegExp)),
+            peerDeps: Joi.boolean(),
           }),
         );
       },
@@ -39,7 +45,7 @@ export default (api: IApi) => {
     );
 
     const config: IConfigs = memo.monorepoRedirect || {};
-    const { exclude = [], srcDir = ['src'] } = config;
+    const { exclude = [], srcDir = ['src'], peerDeps = false } = config;
     // Note: not match `umi` package in local dev
     if (__filename.includes(`packages/preset-umi`)) {
       logger.info(
@@ -47,28 +53,45 @@ export default (api: IApi) => {
       );
       exclude.push(/^umi$/);
     }
-    // collect use workspace deps
+    // collect current project using other workspace deps
     const usingDeps = collectPkgDeps(api.pkg).filter((name) => {
       return !exclude.some((reg) => reg.test(name));
     });
     if (!usingDeps.length) return memo;
     // collect all project
-    const projects = await collectAllProjects({ root });
+    const { projects } = await collectAllProjects({ root });
+    const needRedirectPeerDeps = CORE_DEPS;
     const alias = usingDeps.reduce<Record<string, string>>((obj, name) => {
-      const root = projects[name];
-      if (!root) {
+      const submodule = projects[name];
+      if (!submodule) {
         return obj;
       }
+      // redirect to source dir
       srcDir.some((dirName) => {
-        const dirPath = join(root, dirName);
+        const dirPath = join(submodule.dir, dirName);
         if (existsSync(dirPath) && statSync(dirPath).isDirectory()) {
-          // redirect to source dir
           obj[name] = dirPath;
           return true;
         }
       });
+      // collect peer deps
+      if (peerDeps) {
+        const deps = Object.keys(
+          submodule.packageJson['peerDependencies'] || {},
+        );
+        needRedirectPeerDeps.push(...deps);
+      }
       return obj;
     }, {});
+    // redirect peer deps
+    if (peerDeps) {
+      needRedirectPeerDeps.forEach((depName) => {
+        const depDir = resolveDep({ cwd: api.cwd, name: depName });
+        if (depDir) {
+          alias[depName] = depDir;
+        }
+      });
+    }
     memo.alias = {
       ...memo.alias,
       ...alias,
@@ -94,14 +117,28 @@ function collectPkgDeps(pkg: Record<string, any>) {
 
 async function collectAllProjects(opts: { root: string }) {
   const workspaces = await getPackages(opts.root);
-  return workspaces.packages.reduce<Record<string, string>>(
-    (obj: Record<string, string>, pkg: IProject) => {
+  const projects: Record<string, IProject> = workspaces.packages.reduce(
+    (obj: Record<string, IProject>, pkg: IProject) => {
       const name = pkg.packageJson?.name;
       if (name) {
-        obj[name] = pkg.dir;
+        obj[name] = pkg;
       }
       return obj;
     },
     {},
   );
+  return {
+    projects,
+  };
+}
+
+function resolveDep(opts: { cwd: string; name: string }) {
+  try {
+    const pkgPath = resolve.sync(`${opts.name}/package.json`, {
+      basedir: opts.cwd,
+    });
+    return dirname(pkgPath);
+  } catch {
+    return false;
+  }
 }
