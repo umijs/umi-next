a283170ca0e0426032f0cf0e91bffb5649bcb4b1
Peach <scdzwyxst@gmail.com>
2022-03-24 16:13:17 +0800

    chore(lint): add deps for eslint config

--- packages/lint/src/config/eslint/legacy.ts
+++ packages/lint/src/config/eslint/legacy.ts
@@ -41,10 +41,7 @@ module.exports = {
         '@babel/preset-react',
         '@babel/preset-typescript',
       ],
-      plugins: [
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-      ],
+      plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
     },
     requireConfigFile: false,
     project: TYPE_AWARE_ENABLE ? './tsconfig.json' : undefined,
