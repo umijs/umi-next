c918c01be51d1d71f59896d56da4a30e44116c0a
Peach <scdzwyxst@gmail.com>
2022-03-25 14:12:43 +0800

    feat(lint): add type exports

--- packages/lint/src/index.ts
+++ packages/lint/src/index.ts
@@ -1,10 +1,14 @@
 import { StyleLinter } from './linter';
+import type { ILintArgs } from './types';
 
 // FIXME: test only
 const stylelint = new StyleLinter(process.cwd());
 
 stylelint.run({});
 
-export default () => {
+export type { ILintArgs };
+
+export default (args: ILintArgs) => {
+  args;
   return '@umijs/lint';
 };
---
+++ packages/lint/src/types.ts
@@ -0,0 +1,11 @@
+export interface ILinterOpts {
+  cwd: string;
+  linterResolveDir: string;
+}
+
+export interface ILintArgs {
+  _: string[];
+  fix?: boolean;
+  eslintOnly?: boolean;
+  stylelintOnly?: boolean;
+}
