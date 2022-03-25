006fc4dad79ec56e58ef0fefb8841439c4230104
Peach <scdzwyxst@gmail.com>
2022-03-25 10:39:31 +0800

    chore(lint): add stylelint example

--- packages/lint/src/index.ts
+++ packages/lint/src/index.ts
@@ -1,3 +1,10 @@
+import { StyleLinter } from './linter';
+
+// FIXME: test only
+const stylelint = new StyleLinter(process.cwd());
+
+stylelint.run({});
+
 export default () => {
   return '@umijs/lint';
 };
