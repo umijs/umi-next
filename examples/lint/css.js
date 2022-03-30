8a6ad4d63fc3019d1b9208b74a7dff6b430218e0
晚兮 <wh365686@alibaba-inc.com>
2022-03-30 12:00:34 +0800

    feat(lint): add cssinjs example

---
+++ examples/lint/css.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+const StyledHeader = styled.header`
+  widt: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+`;
