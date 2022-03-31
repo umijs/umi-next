e0bf30ed8eb269bd5ba2caf762aa16bb5a39a5a7
晚兮 <wh365686@alibaba-inc.com>
2022-03-31 15:07:13 +0800

    feat(lint): fix docs

--- packages/lint/src/index.ts
+++ packages/lint/src/index.ts
@@ -1,14 +1,21 @@
 import { EsLinter, StyleLinter } from './linter';
 import type { ILintArgs, ILinterOpts } from './types';
 
+const ES_EXTS = ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'];
+const STYLE_EXTS = [
+  '**/*.less',
+  '**/*.css',
+  '**/*.sass',
+  '**/*.scss',
+  '**/*.styl',
+];
 export type { ILintArgs, ILinterOpts };
 
 export default (opts: ILinterOpts, args: ILintArgs) => {
   if (!args.eslintOnly) {
     const stylelint = new StyleLinter(opts);
-    const STYLE_EXTS = ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'];
     if (!args.cssinjs) {
-      for (const suffix of STYLE_EXTS) {
+      for (const suffix of ES_EXTS) {
         args._.unshift('--ignore-pattern', suffix);
       }
     }
@@ -17,14 +24,7 @@ export default (opts: ILinterOpts, args: ILintArgs) => {
 
   if (!args.stylelintOnly) {
     const eslint = new EsLinter(opts);
-    const ES_EXTS = [
-      '**/*.less',
-      '**/*.css',
-      '**/*.sass',
-      '**/*.scss',
-      '**/*styl',
-    ];
-    for (const suffix of ES_EXTS) {
+    for (const suffix of STYLE_EXTS) {
       args._.unshift('--ignore-pattern', suffix);
     }
     eslint.run(args);
