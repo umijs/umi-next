cf2d59744a2531b0a10bea1163fe023e88224f1d
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-04-01 16:19:34 +0800

    wip(ssr): Add styles, svg and images import in ssr-demo

--- examples/ssr-demo/src/pages/index.tsx
+++ examples/ssr-demo/src/pages/index.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { Link, useClientLoaderData, useLoaderData } from 'umi';
 import Button from '../components/Button';
 import connect from '../database/connect';
+import './index.less';
+// @ts-ignore
+import styles from './index.less';
+// @ts-ignore
+import SmileUrl, { ReactComponent as SVGSmile } from './smile.svg';
+// @ts-ignore
+import umiLogo from './umi.png';
 
 export default function HomePage() {
   const loaderData = useLoaderData();
   const clientLoaderData = useClientLoaderData();
   return (
     <div>
-      <h1>Hello~</h1>
-      <p>This is index.tsx</p>
+      <h1 className="title">Hello~</h1>
+      <p className={styles.blue}>This is index.tsx</p>
+      <img src={SmileUrl} alt="smile" />
+      <SVGSmile />
       <Button />
+      <img src={umiLogo} alt="umi" />
       <Link to="/users/user">/users/user</Link>
       <p>loader data: {JSON.stringify(loaderData)}</p>
       <p>client loader data: {JSON.stringify(clientLoaderData)}</p>
@@ -20,7 +30,6 @@ export default function HomePage() {
 
 export async function loader() {
   connect();
-  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
   return { message: 'data from server loader of index.tsx' };
 }
 
