af1da266613eb14af8aeb2dbd94ae600f750f537
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-04-06 13:51:37 +0800

    fix(api-route): Fix bug of api route that overwrite cwd.api directory

--- packages/preset-umi/src/features/apiRoute/constants.ts
+++ packages/preset-umi/src/features/apiRoute/constants.ts
@@ -1,1 +1,34 @@
-export const OUTPUT_PATH = 'api';
+import { join } from 'path';
+import type { IApi } from '../../types';
+
+export enum ServerlessPlatform {
+  Vercel = 'vercel',
+  Netlify = 'netlify',
+  Worker = 'worker',
+  UmiDevServer = 'umi-dev-server',
+}
+
+export function getPlatform(p: string) {
+  switch (p) {
+    case 'vercel':
+      return ServerlessPlatform.Vercel;
+    case 'netlify':
+      return ServerlessPlatform.Netlify;
+    case 'worker':
+      return ServerlessPlatform.Worker;
+    default:
+      return undefined;
+  }
+}
+
+// Get the final output path of the api route files
+export function getApiRouteBuildPath(api: IApi, platform: ServerlessPlatform) {
+  switch (platform) {
+    case ServerlessPlatform.Vercel:
+      return join(api.paths.cwd, 'api');
+    case ServerlessPlatform.UmiDevServer:
+      return join(api.paths.absTmpPath, 'api/_compiled');
+    default:
+      return join(api.paths.cwd, 'api');
+  }
+}
