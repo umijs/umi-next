d114f08e68b24abc28cdcda0230798debba77694
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-04-06 11:17:08 +0800

    wip(ssr): Skip css injection if esbuild has errors

--- packages/preset-umi/src/features/ssr/ssr.ts
+++ packages/preset-umi/src/features/ssr/ssr.ts
@@ -65,7 +65,8 @@ export default (api: IApi) => {
             // 打包完成后，除了 umi.server.js 外，如果项目中有引入样式资源，
             // 还会生成 umi.server.css 文件，我们需要将这个文件写入到 umi.server.ts 中
             // 将 css 写入 umi.server.js 后就可以把 css 构建产物删除了
-            build.onEnd(() => {
+            build.onEnd((result) => {
+              if (result.errors.length > 0) return;
               const css = readFileSync(
                 absServerBuildPath(api).replace(/\.js$/, '.css'),
               );
