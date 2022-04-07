4fa7c95cb0ee2095b286b5256605d8ab5ca5945b
lylwanan <lyl444@qq.com>
2022-04-07 15:59:44 +0800

    feat(father): add babel transformer

--- packages/father/src/builder/bundless/loaders/javascript/babel.ts
+++ packages/father/src/builder/bundless/loaders/javascript/babel.ts
@@ -1,5 +1,13 @@
-import { IFatherJSTransformerTypes } from '../../../../types';
+import {
+  transform as babelTransform,
+  TransformOptions,
+} from '@umijs/bundler-utils/compiled/babel/core';
+import {
+  IFatherJSTransformerTypes,
+  IFatherPlatformTypes,
+} from '../../../../types';
 import type { IBundlessConfig } from '../../../config';
+import resolvePath from './resolvePath';
 
 /**
  * babel transformer
@@ -7,14 +15,81 @@ import type { IBundlessConfig } from '../../../config';
 class BabelTransformer {
   static id = IFatherJSTransformerTypes.BABEL;
 
+  config = {} as TransformOptions;
+
   constructor(config: IBundlessConfig) {
-    config;
     // TODO: create babel instance from config
+    this.config = this.getBabelConfig(config);
   }
 
-  process(content: string) {
-    // TODO: transform content
-    return content;
+  getRelativePath() {}
+
+  getBabelConfig(config: IBundlessConfig): TransformOptions {
+    const { platform, extraBabelPlugins, extraBabelPresets, define, alias } =
+      config;
+    const isBrowser = platform === IFatherPlatformTypes.BROWSER;
+
+    return {
+      presets: [
+        [
+          require.resolve(
+            '@umijs/bundler-utils/compiled/babel/preset-typescript',
+          ),
+          {
+            allowNamespaces: true,
+            allowDeclareFields: true,
+            onlyRemoveTypeImports: true,
+            optimizeConstEnums: true,
+          },
+        ],
+        [
+          require.resolve('@umijs/bundler-utils/compiled/babel/preset-env'),
+          {
+            targets: { browsers: ['last 2 versions', 'IE 11'] },
+            modules: false,
+          },
+        ],
+        ...(isBrowser
+          ? [
+              require.resolve(
+                '@umijs/bundler-utils/compiled/babel/preset-react',
+              ),
+            ]
+          : []),
+        ...(extraBabelPresets ? extraBabelPresets : []),
+      ],
+      plugins: [
+        [require.resolve('babel-plugin-transform-define'), define || {}],
+        ...(isBrowser ? [require.resolve('babel-plugin-react-require')] : []),
+        [
+          require.resolve(
+            '@umijs/bundler-utils/compiled/babel/plugin-transform-runtime',
+          ),
+          {
+            helpers: true,
+            regenerator: true,
+            useESModules: false,
+          },
+        ],
+        [
+          require.resolve('babel-plugin-module-resolver'),
+          {
+            alias: alias || {},
+            resolvePath,
+          },
+        ],
+        ...(extraBabelPlugins ? extraBabelPlugins : []),
+      ],
+    };
+  }
+
+  process(content: string, fileAbsPath: string) {
+    const config = this.config;
+
+    return babelTransform(content, {
+      filename: fileAbsPath,
+      ...config,
+    })?.code;
   }
 }
 
