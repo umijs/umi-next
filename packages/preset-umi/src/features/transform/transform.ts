ae12a22fc8c082e63f019b3118d90bca02ba0262
PeachScript <scdzwyxst@gmail.com>
2022-04-21 00:00:47 +0800

    feat(preset-umi): handle CodeFrameError in onCheckCode

--- packages/preset-umi/src/features/transform/transform.ts
+++ packages/preset-umi/src/features/transform/transform.ts
@@ -1,3 +1,5 @@
+import { codeFrameColumns } from '@umijs/bundler-utils/compiled/babel/code-frame';
+import { chalk } from '@umijs/utils';
 import { IApi } from '../../types';
 import babelPlugin from './babelPlugin';
 import CodeFrameError from './CodeFrameError';
@@ -13,14 +15,32 @@ export default (api: IApi) => {
               cwd: api.cwd,
               absTmpPath: api.paths.absTmpPath,
               onCheckCode({ args }: any) {
-                api.applyPlugins({
-                  key: 'onCheckCode',
-                  args: {
-                    ...args,
-                    CodeFrameError,
-                  },
-                  sync: true,
-                });
+                try {
+                  api.applyPlugins({
+                    key: 'onCheckCode',
+                    args: {
+                      ...args,
+                      CodeFrameError,
+                    },
+                    sync: true,
+                  });
+                } catch (err) {
+                  if (err instanceof CodeFrameError) {
+                    // throw with babel code frame error
+                    throw new Error(
+                      `\n${codeFrameColumns(args.code, err.location, {
+                        highlightCode: true,
+                        message: err.message,
+                      })}\n`,
+                    );
+                  } else if (err instanceof Error) {
+                    // throw normal error with red message
+                    throw new Error(chalk.redBright(err.message));
+                  } else {
+                    // log error
+                    api.logger.error(err);
+                  }
+                }
               },
             },
           ],
