4f1210a8a3d2bd6efd657900917f914ae4143e78
PeachScript <scdzwyxst@gmail.com>
2022-04-26 18:38:12 +0800

    refactor(bundler-vite): remove error extraBabelIncludes consumer

--- packages/bundler-vite/src/schema.ts
+++ packages/bundler-vite/src/schema.ts
@@ -20,7 +20,6 @@ export function getSchemas(): Record<string, (Joi: Root) => any> {
       ),
     define: (Joi) => Joi.object(),
     externals: (Joi) => Joi.object().pattern(/^/, Joi.string()),
-    extraBabelIncludes: (Joi) => Joi.array().items(Joi.string()),
     extraBabelPlugins: (Joi) =>
       Joi.alternatives().try(
         Joi.string(),
