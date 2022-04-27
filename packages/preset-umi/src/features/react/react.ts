97cd064fcaa83442d7437d1fd4479390676f7de3
PeachScript <scdzwyxst@gmail.com>
2022-04-27 18:36:53 +0800

    refactor(preset-umi): use modifyConfig

--- packages/preset-umi/src/features/react/react.ts
+++ packages/preset-umi/src/features/react/react.ts
@@ -2,18 +2,21 @@ import { semver } from '@umijs/utils';
 import type { IApi } from '../../types';
 
 export default (api: IApi) => {
-  api.chainWebpack((memo) => {
-    const isLTEReact18 = semver.lte(api.appData.react.version, '18.0.0');
+  api.modifyConfig((memo) => {
+    // greater than range
+    const isReact18GTR =
+      api.pkg.dependencies?.react &&
+      semver.gtr('18.0.0', api.pkg.dependencies.react);
 
     // compatible with < react@18 for @umijs/renderer-react
-    if (isLTEReact18) {
-      const reactDOM = memo.resolve.alias.get('react-dom');
+    if (isReact18GTR) {
+      const reactDOM = memo.alias['react-dom'];
 
-      memo.resolve.alias.set('react-dom/client$', reactDOM);
+      memo.alias['react-dom/client'] = reactDOM;
 
       // put react-dom after react-dom/client
-      memo.resolve.alias.delete('react-dom');
-      memo.resolve.alias.set('react-dom', reactDOM);
+      delete memo.alias['react-dom'];
+      memo.alias['react-dom'] = reactDOM;
     }
 
     return memo;
