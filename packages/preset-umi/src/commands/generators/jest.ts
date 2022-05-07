765288b5243246cd37bf2a851e8f945b61bfdea8
pshu <pishu.spf@antfin.com>
2022-05-07 10:16:24 +0800

    refactor: 🎨 better reading regex

--- packages/preset-umi/src/commands/generators/jest.ts
+++ packages/preset-umi/src/commands/generators/jest.ts
@@ -59,7 +59,7 @@ export default async () => {
       target: 'browser',
     }),
     // if you require some es-module npm package, please uncomment below line and insert your package name
-    // transformIgnorePatterns: ['node_modules/(?!(.*lodash-es|.*your-es-pkg-name))']
+    // transformIgnorePatterns: ['node_modules/(?!.*(lodash-es|your-es-pkg-name)/)']
   })) as Config.InitialOptions;
 };
 `.trimLeft(),
