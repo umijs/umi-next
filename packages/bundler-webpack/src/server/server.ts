1ff14a6e68b8a8c02a01e0a907ee6dbc40ee72b6
pshu <pishu.spf@antfin.com>
2022-05-19 22:55:25 +0800

    feat: ✨ make all .js file can be replaced and debug in dev mode

--- packages/bundler-webpack/src/server/server.ts
+++ packages/bundler-webpack/src/server/server.ts
@@ -7,7 +7,7 @@ import webpack, {
 import { chalk, logger } from '@umijs/utils';
 import { createReadStream, existsSync } from 'fs';
 import http from 'http';
-import { join } from 'path';
+import { extname, join } from 'path';
 import { MESSAGE_TYPE } from '../constants';
 import { IConfig } from '../types';
 import { createWebSocketServer } from './ws';
@@ -53,9 +53,12 @@ export async function createServer(opts: IOpts) {
 
   // TODO: add to before middleware
   app.use((req, res, next) => {
-    if (req.path === '/umi.js' && existsSync(join(opts.cwd, 'umi.js'))) {
-      res.setHeader('Content-Type', 'application/javascript');
-      createReadStream(join(opts.cwd, 'umi.js')).on('error', next).pipe(res);
+    const file = req.path;
+    const filePath = join(opts.cwd, file);
+    const ext = extname(filePath);
+
+    if (ext === '.js' && existsSync(filePath)) {
+      res.sendFile(filePath);
     } else {
       next();
     }
