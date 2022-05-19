e8f34564ef07c80e10e98a75f2af5af3cb837c50
fz6m <59400654+fz6m@users.noreply.github.com>
2022-05-19 22:55:46 +0800

    feat(on-demand): parcel css and uglify-js install on demand

--- packages/preset-umi/src/features/depsOnDemand/depsOnDemand.ts
+++ packages/preset-umi/src/features/depsOnDemand/depsOnDemand.ts
@@ -1,40 +1,75 @@
-import { fsExtra, installWithNpmClient } from '@umijs/utils';
+import { chalk, fsExtra, installWithNpmClient } from '@umijs/utils';
 import { existsSync } from 'fs';
 import { join } from 'path';
 import { IApi } from '../../types';
 
+interface ICheckDep {
+  name: string;
+  condition: boolean;
+}
+
+const DEPS_MAP: Record<string, string> =
+  require('@umijs/bundler-webpack/package.json').devDependencies;
+
 export default (api: IApi) => {
+  // TODO: 支持在上层框架锁定，比如通过 api.appData.depsOnDemand 添加可选依赖
+  const hasDep = (name: string) => {
+    return api.pkg.dependencies?.[name] || api.pkg.devDependencies?.[name];
+  };
+  const checkDeps = (deps: ICheckDep[]) => {
+    const willInstallDeps: string[] = [];
+    deps.forEach((dep) => {
+      const { name, condition } = dep;
+      if (condition && !hasDep(name)) {
+        const nameWithColor = chalk.cyan(name);
+        api.logger.info(`
+ > Since ${nameWithColor} is used, install ${nameWithColor} on demand.`);
+        willInstallDeps.push(name);
+      }
+    });
+    if (!willInstallDeps.length) return;
+    addDeps({
+      pkgPath: api.pkgPath || join(api.cwd, 'package.json'),
+      deps: willInstallDeps,
+    });
+    installWithNpmClient({
+      npmClient: api.appData.npmClient,
+      cwd: api.cwd,
+    });
+  };
+
   api.onStart(() => {
-    // TODO: 支持在上层框架锁定，比如通过 api.appData.depsOnDemand 添加可选依赖
+    // swc
     const hasSwcConfig =
       api.config.srcTranspiler === 'swc' || api.config.depTranspiler === 'swc';
-    const swcInstalled =
-      api.pkg.dependencies?.['@swc/core'] ||
-      api.pkg.devDependencies?.['@swc/core'];
-    if (hasSwcConfig && !swcInstalled) {
-      api.logger.info('Since swc is used, install @swc/core on demand.');
-      addDeps({
-        pkgPath: api.pkgPath || join(api.cwd, 'package.json'),
+    // uglify-js
+    const hasUglifyJsConfig = api.config.jsMinifier === 'uglifyJs';
+    // parcel-css
+    const hasParcelCSSConfig = api.config.cssMinifier === 'parcelCSS';
+
+    checkDeps([
+      {
         name: '@swc/core',
-        version: `^${
-          require('@umijs/bundler-webpack/package.json').devDependencies[
-            '@swc/core'
-          ]
-        }`,
-      });
-      installWithNpmClient({
-        npmClient: api.appData.npmClient,
-        cwd: api.cwd,
-      });
-    }
+        condition: hasSwcConfig,
+      },
+      {
+        name: 'uglify-js',
+        condition: hasUglifyJsConfig,
+      },
+      {
+        name: '@parcel/css',
+        condition: hasParcelCSSConfig,
+      },
+    ]);
   });
 };
 
-function addDeps(opts: { pkgPath: string; name: string; version: string }) {
-  const pkg = existsSync(opts.pkgPath)
-    ? fsExtra.readJSONSync(opts.pkgPath)
-    : {};
-  pkg.devDependencies = pkg.devDependencies || {};
-  pkg.devDependencies[opts.name] = opts.version;
-  fsExtra.writeJSONSync(opts.pkgPath, pkg, { spaces: 2 });
+function addDeps(opts: { pkgPath: string; deps: string[] }) {
+  const { pkgPath, deps } = opts;
+  const pkg = existsSync(pkgPath) ? fsExtra.readJSONSync(pkgPath) : {};
+  pkg.devDependencies ||= {};
+  deps.forEach((name) => {
+    pkg.devDependencies[name] = `^${DEPS_MAP[name]}`;
+  });
+  fsExtra.writeJSONSync(pkgPath, pkg, { spaces: 2 });
 }
