6575ee6c745371a41d0f6147b7792e3418494e32
fz6m <59400654+fz6m@users.noreply.github.com>
2022-05-20 02:31:09 +0800

    fix: tsc

--- packages/preset-umi/src/features/monorepo/redirect.ts
+++ packages/preset-umi/src/features/monorepo/redirect.ts
@@ -1,9 +1,9 @@
-// @ts-ignore
 import { isUmiLocal, logger } from '@umijs/utils';
 import { pkgUp } from '@umijs/utils/compiled/pkg-up';
 import assert from 'assert';
 import { existsSync, statSync } from 'fs';
 import { dirname, join } from 'path';
+// @ts-ignore
 import { getPackages } from '../../../compiled/@manypkg/get-packages';
 import type { IApi } from '../../types';
 
