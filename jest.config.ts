67194f2b86f09b93299ba69f3d20a7748670944a
pshu <pishu.spf@antfin.com>
2022-05-23 11:39:52 +0800

    chore(jest): 🔧 test 匹配的正则更具体

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import { Config, createConfig } from 'umi/test';
 
 export default {
   ...createConfig(),
-  testMatch: ['**/packages/*/src/**/*.test.ts'],
+  testMatch: ['<rootDir>/packages/*/src/**/*.test.ts'],
   modulePathIgnorePatterns: [
     '<rootDir>/packages/.+/compiled',
     '<rootDir>/packages/.+/fixtures',
