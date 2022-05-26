48f95c5c49e8bf9945b00c3ca54ac5532393ce38
pshu <pishu.spf@antfin.com>
2022-05-26 23:36:30 +0800

    feat: ✨ add more hooks in buildDepPlugin

--- packages/mfsu/src/webpackPlugins/buildDepPlugin.ts
+++ packages/mfsu/src/webpackPlugins/buildDepPlugin.ts
@@ -2,6 +2,8 @@ import type { Compiler } from 'webpack';
 
 interface IOpts {
   onCompileDone: Function;
+  onFileChange?: (c: Compiler) => Promise<any>;
+  beforeCompile?: () => Promise<any>;
 }
 
 const PLUGIN_NAME = 'MFSUBuildDeps';
@@ -14,7 +16,15 @@ export class BuildDepPlugin {
   }
 
   apply(compiler: Compiler): void {
+    compiler.hooks.watchRun.tapPromise(PLUGIN_NAME, (c) => {
+      return this.opts.onFileChange?.(c) || Promise.resolve();
+    });
+
     compiler.hooks.beforeCompile.tap(PLUGIN_NAME, () => {
+      this.opts.beforeCompile?.();
+    });
+
+    compiler.hooks.compile.tap(PLUGIN_NAME, () => {
       this.opts.onCompileDone();
     });
   }
