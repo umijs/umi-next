97f18ee2e046bd47dac19eb8299d4daf89b2b3ec
xierenyuan <xierenyuan@qq.com>
2022-06-23 00:36:08 +0800

    test(preset-umi): update the  getRoutes test case

--- packages/preset-umi/src/features/tmpFiles/routes.test.ts
+++ packages/preset-umi/src/features/tmpFiles/routes.test.ts
@@ -44,8 +44,22 @@ test('getRoutes', async () => {
     } as any,
   });
 
+  // login 页不包含布局
   expect(routes[1].file).toBe('@/pages/users/Login/index.tsx');
   expect(routes[1].parentId).toBe(undefined);
+
+  // @@/global-layout
+  expect(routes['@@/global-layout'].file).toBe('@/layouts/index.tsx');
+  expect(routes['@@/global-layout'].parentId).toBe(undefined);
+  expect(routes['@@/global-layout'].isLayout).toBe(true);
+
+  // 普通页面增加布局
+  expect(routes[2].file).toBe('@/pages/404.tsx');
+  expect(routes[2].parentId).toBe('@@/global-layout');
+
+  // vue 文件测试
+  expect(routes[3].file).toBe('@/pages/Hello/index.vue');
+  expect(routes[3].parentId).toBe('@@/global-layout');
 
   // __absFile 是具体的路径, 快照测试通不过
   Object.keys(routes).forEach((id) => {
